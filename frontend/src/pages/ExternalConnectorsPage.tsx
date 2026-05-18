a4108ab33fefc2d90106cc6433e5d4a05a47cf35
Hiroaki Nakashima <h-nakashima@local>
2026-05-18 10:32:30 +0900

    fix: remove extra brace in ExternalConnectorsPage

--- frontend/src/pages/ExternalConnectorsPage.tsx
+++ frontend/src/pages/ExternalConnectorsPage.tsx
@@ -309,7 +309,7 @@ const ExternalConnectorsPage: React.FC = () => {
         </DialogContent>
       </Dialog>
 
-      {/* 追加/編集ダイアログ */}}
+      {/* 追加/編集ダイアログ */}
       <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
         <DialogContent className="max-w-lg max-h-[90vh] overflow-y-auto">
           <DialogHeader>
